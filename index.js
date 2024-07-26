239bd577a1698e85f4da0dafbfe36572830c1121
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-26 20:00:14 -0300

    index e inicio do js nos botôes modal

---
+++ index.js
@@ -0,0 +1,8 @@
+function cartOpen() {
+  let modal = document.querySelector("div.modal-container");
+  modal.style.display = "flex";
+}
+
+function cartClose(){
+    document.querySelector('div.modal-container').style.display='none'
+}
