058932797831ba64dd020ffae1739561fa6d06e9
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-26 20:32:11 -0300

    add list events

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-function cartOpen() {
-  let modal = document.querySelector("div.modal-container");
-  modal.style.display = "flex";
-}
+const menu = document.getElementById('menu')
+const modalContainer = document.getElementById('modal-container')
+const cartBtn = document.getElementById('cart-btn')
+const cartItemContainer = document.querySelector('div#cart-items')
+const total = document.getElementById('cart-total')
+const checkOut = document.getElementById('modal-finish-btn')
+const closeModalBtn = document.getElementById('modal-close-btn')
+const cartCounter = document.getElementById('cart-count')
+const address = document.getElementById('address')
+const adressWarn = document.getElementById('address-warn')
 
-function cartClose(){
-    document.querySelector('div.modal-container').style.display='none'
-}
+
+
+cartBtn.addEventListener("click", function(){
+    modalContainer.style.display = 'flex'
+})
+
+closeModalBtn.addEventListener("click", function(){
+    modalContainer.style.display = 'none'
+})
+
